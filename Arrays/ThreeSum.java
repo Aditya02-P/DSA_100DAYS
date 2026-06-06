5929e5876c0fdb361f8acb01955f2af8f0b3af65
Aditya02-P <pradhanaditya0725@gmail.com>
2026-06-06 21:48:20 +0530

    Added 6-6-26 learnings

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 public class ThreeSum {
     public static void main(String[] args) {
         int[]arr={-1,0,-1,2,-1,-4};
@@ -19,8 +22,57 @@ public class ThreeSum {
         }
     }
 
-    public static void twoSum1(int[] arr,int target){
-        int sum=0;
+    public static void threeSum1(int[] arr,int target){
 
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(
+                            nums[i],
+                            nums[left],
+                            nums[right]
+                    ));
+
+                    left++;
+                    right--;
+
+                    // Skip duplicates
+                    while (left < right &&
+                            nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right &&
+                            nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
     }
 }
