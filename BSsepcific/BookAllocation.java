b03d2d31d52dee0254723bcb1c32e680755dfa2d
Aditya02-P <pradhanaditya0725@gmail.com>
2026-06-30 11:45:23 +0530

    Addd 29,30 -6-26 learnings

---
+++ BSsepcific/BookAllocation.java
@@ -0,0 +1,79 @@
+public class BookAllocation {
+
+    public static void main(String[] args) {
+        int[] pages = {25, 46, 28, 49, 24};
+        int students = 4;
+
+        int ans = allocateBooks(pages, students);
+        System.out.println("Minimum possible maximum pages: " + ans);
+    }
+
+    public static int allocateBooks(int[] pages, int students) {
+
+        if (students > pages.length) {
+            return -1;
+        }
+
+        int low = findMax(pages);
+        int high = findSum(pages);
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canAllocate(pages, students, mid)) {
+                ans = mid;
+                high = mid - 1; // Try for a smaller maximum
+            } else {
+                low = mid + 1; // Increase the limit
+            }
+        }
+
+        return ans;
+    }
+
+    public static boolean canAllocate(int[] pages, int students, int maxPages) {
+
+        int studentCount = 1;
+        int pageSum = 0;
+
+        for (int page : pages) {
+
+            if (pageSum + page <= maxPages) {
+                pageSum += page;
+            } else {
+                studentCount++;
+                pageSum = page;
+
+                if (studentCount > students) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static int findMax(int[] pages) {
+        int max = pages[0];
+
+        for (int page : pages) {
+            if (page > max) {
+                max = page;
+            }
+        }
+
+        return max;
+    }
+
+    public static int findSum(int[] pages) {
+        int sum = 0;
+
+        for (int page : pages) {
+            sum += page;
+        }
+
+        return sum;
+    }
+}
