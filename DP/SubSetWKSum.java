81627d02f48973fa052f921e33a09af591c8df95
Aditya02-P <pradhanaditya0725@gmail.com>
2026-07-22 23:06:15 +0530

    Added learnings

--- DP/SubSetWKSum.java
+++ DP/SubSetWKSum.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 
 public class SubSetWKSum {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int k = 9;;
+        int[] arr = {0  ,1,2,3,4,5};
+        int k = 3;
 
         Boolean[][] dp =  new Boolean[arr.length+1][k+1];
 
-
-        boolean ans = findSubset(arr,dp,arr.length-1,k);
+        int ans = countSubsets(arr,k);
 
         System.out.println(ans);
     }
@@ -22,7 +21,7 @@ public class SubSetWKSum {
         }
 
         if(dp[i][k]!=null){
-            return true;
+            return dp[i][k];
         }
 
         dp[i][k]=findSubset(arr,dp,i-1,k-arr[i]) || findSubset(arr,dp,i-1,k);
@@ -37,11 +36,150 @@ public class SubSetWKSum {
         if(k==0){
             return true;
         }
-
-
         return checkSubsetWS(arr,k-arr[i],i-1) || checkSubsetWS(arr,k,i-1);
     }
 
+    //BottomUp Approach
+
+    private static boolean findSubsetWS(int[] arr, int k) {
+        int[][] dp = new int[arr.length][k+1];
+        for(int i=0;i<arr.length;i++){
+            dp[i][0]=1;
+        }
+        if (arr[0] <= k)
+            dp[0][arr[0]] = 1;
+
+        for(int i=1;i< arr.length;i++){
+            for(int j=1;j<=k;j++){
+                if(arr[i]<=j)
+                    dp[i][j] =  (dp[i - 1][j] > 0 || dp[i - 1][j - arr[i]] > 0) ? 1 : 0;
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[arr.length-1][k]==1;
+    }
+
+    private static boolean subsetSum(int[] arr, int k) {
+
+        boolean[][] dp = new boolean[arr.length][k + 1];
+
+        // Sum 0 is always possible
+        for (int i = 0; i < arr.length; i++)
+            dp[i][0] = true;
+
+        // First element
+        if (arr[0] <= k)
+            dp[0][arr[0]] = true;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            for (int target = 1; target <= k; target++) {
+
+                boolean notTake = dp[i - 1][target];
+
+                boolean take = false;
+
+                if (arr[i] <= target)
+                    take = dp[i - 1][target - arr[i]];
+
+                dp[i][target] = take || notTake;
+            }
+        }
+
+        return dp[arr.length - 1][k];
+    }
+
+    private static int countSubsets(int[] arr, int k) {
+
+        int[][] dp = new int[arr.length][k + 1];
+
+        for (int i = 0; i < arr.length; i++)
+            dp[i][0] = 1;
+
+        if (arr[0] <= k)
+            dp[0][arr[0]] = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            for (int j = 1; j <= k; j++) {
+
+                int notTake = dp[i - 1][j];
+
+                int take = 0;
+
+                if (arr[i] <= j)
+                    take = dp[i - 1][j - arr[i]];
+
+                dp[i][j] = take + notTake;
+            }
+        }
+
+        return dp[arr.length - 1][k];
+    }
+
+    private static int countSubsetsWZero    (int[] arr, int target) {
+
+        int n = arr.length;
+        int[][] dp = new int[n][target + 1];
+
+        // Base case
+        if (arr[0] == 0) {
+            dp[0][0] = 2;      // {} and {0}
+        } else {
+            dp[0][0] = 1;      // {}
+            if (arr[0] <= target)
+                dp[0][arr[0]] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            for (int sum = 0; sum <= target; sum++) {
+
+                int notTake = dp[i - 1][sum];
+
+                int take = 0;
+
+                if (arr[i] <= sum)
+                    take = dp[i - 1][sum - arr[i]];
+
+                dp[i][sum] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][target];
+    }
+
+    private static boolean subsetSumSpaceOptimized(int[] arr, int k) {
+
+        boolean[] prev = new boolean[k + 1];
+        prev[0] = true;
+
+        if (arr[0] <= k)
+            prev[arr[0]] = true;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            boolean[] curr = new boolean[k + 1];
+            curr[0] = true;
+
+            for (int sum = 1; sum <= k; sum++) {
+
+                boolean notTake = prev[sum];
+
+                boolean take = false;
+                if (arr[i] <= sum)
+                    take = prev[sum - arr[i]];
+
+                curr[sum] = take || notTake;
+            }
+
+            prev = curr;
+        }
+
+        return prev[k];
+    }
 
 
 
